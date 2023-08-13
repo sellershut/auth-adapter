e1b9967b910174e7ca54e03c77061e6112601f35
rtkay123 <dev@kanjala.com>
2023-08-13 20:25:01 +0200

    fix: add delete session endpoint

--- src/open_api.rs
+++ src/open_api.rs
@@ -1,6 +1,6 @@
 use crate::routes::{
     __path_create_account, __path_create_session, __path_create_user, __path_create_verif_token,
-    __path_delete_account, __path_delete_user, __path_delete_verif_token,
+    __path_delete_account, __path_delete_session, __path_delete_user, __path_delete_verif_token,
     __path_get_session_and_user, __path_get_users, __path_update_session, __path_update_user,
 };
 use entities::utoipa::OpenApi;
@@ -18,6 +18,7 @@ use entities::{account::Model as Account, session::Model as Session, user::Model
         create_session,
         get_session_and_user,
         update_session,
+        delete_session,
         delete_verif_token,
         create_verif_token
     ),
